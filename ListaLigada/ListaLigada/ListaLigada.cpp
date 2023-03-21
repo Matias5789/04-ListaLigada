7ac3ae55d60d641a191a6c1dfcd6b51df32faa67
mateus.santos105 <mateusrjm4@gamail.com>
2023-03-21 14:44:08 -0300

    finalizado!!!

--- ListaLigada/ListaLigada/ListaLigada.cpp
+++ ListaLigada/ListaLigada/ListaLigada.cpp
@@ -128,6 +128,13 @@ void inserirElemento()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
+	NO* jaExiste = posicaoElemento(novo->valor);
+
+	if (jaExiste) {
+		cout << "numero já colocado na lista!" << endl;
+		return;
+	}
+
 	if (primeiro == NULL)
 	{
 		primeiro = novo;
@@ -145,13 +152,35 @@ void inserirElemento()
 
 void excluirElemento()
 {
-	
+	int del;
+	cout << "digite o numero a ser excluido:";
+	cin >> del;
+	NO* delet = posicaoElemento(del);
+	if (delet == primeiro) {
+		primeiro = primeiro->prox;
+	}
+	else if (delet != NULL) {
+		NO* aux = primeiro;
+		while (aux->prox != delet) {
+			aux = aux->prox;
+		}
+		aux->prox = delet->prox;
+		free(delet);
+	}
 }
 
 void buscarElemento()
 {
-	
+	int numero;
+	cout << "digite o numero a ser buscado:" << endl;
+	cin >> numero;
+	if (posicaoElemento(numero)==NULL) {
+		cout << "nope" << endl;
 }
+	else {
+		cout << "tem" << endl;
+	}
+	}
 
 
 
